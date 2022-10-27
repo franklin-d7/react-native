5e97da5e141407fffc5a71ee7219002d5f2af707
Franklin Rochina <frochina.1999@gmail.com>
2022-10-27 18:12:18 -0500

    get posts

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,55 @@
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { ResultPost } from "../interface/Post";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+
+  const [post, setPost] = useState([])
+
+  const getPosts = async  ()  => {
+    
+
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const { results } = await response.json();
+
+    setPost(results);
+    return results;
+
+  }
+
+  useEffect(() => {
+
+    getPosts()
+    
+  },[] )
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>CHARACTER LIST</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>CHARACTER LIST </Text>
+   
+      <FlatList
+        data={ post }
+        renderItem={({ item }) => (
+            <Text>{item.name}</Text>      
+        )
+         
+      }
+      />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -17,16 +58,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
